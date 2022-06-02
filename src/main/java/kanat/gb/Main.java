1b58801991f7d862796f1cd79142e0b8d7f8de1f
Kanat <abdsattar02@inbox.ru>
2022-06-02 17:02:36 +0600

    3 step

--- src/main/java/kanat/gb/Main.java
+++ src/main/java/kanat/gb/Main.java
@@ -6,36 +6,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 
 public class Main extends JFrame {
+    private final Scanner scan = new Scanner(System.in);
+
     private JLabel display;
+    private JLabel displayAns;
     public Main() {
 
-        setTitle("Calculator");
+        setTitle("Chat");
         setBounds(300, 300, 450, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+        final JLabel text = new JLabel("Enter text: ");
+        final JPanel sendText = new JPanel();
+        final JTextField tf = new JTextField(20);
 
         final ActionListener actionListener = new ActionListener(){
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                final JTextField message = (JTextField) e.getSource();
-                final String text = message.getText();
-
-                String displayText = display.getText();
-                if("Your message".equals(displayText)){
-                    displayText = "";
-
-                }
-                displayText += text;
-                display.setText(displayText);
-
+                final JButton source = (JButton) e.getSource();
+                String str = tf.getText();
+                display.setText(str);
+                System.out.println(str);
+                String ans = scan.toString();
             }
         };
 
-        final JLabel text = new JLabel("Enter text: ");
-        final JPanel sendText = new JPanel();
-        final JTextField tf = new JTextField(20);
         final JButton send = new JButton("SEND");
         send.addActionListener(actionListener);
 
@@ -52,12 +52,16 @@ public class Main extends JFrame {
 
         display = new JLabel("Your message");
         display.setHorizontalAlignment(SwingConstants.RIGHT);
-        display.setFont(new Font("Arial",Font.BOLD,20));
+        display.setFont(new Font("Serif",Font.PLAIN,15));
 
-        send.setActionCommand(display.getText());
+
+        displayAns = new JLabel("Answer");
+        displayAns.setHorizontalAlignment(SwingConstants.LEFT);
+        displayAns.setFont(new Font("Serif",Font.PLAIN,15));
+
         add(display, BorderLayout.NORTH);
         add(sendText, BorderLayout.SOUTH);
-        add(ta);
+
 
 
 
